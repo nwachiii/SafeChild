47a3654ee2c5738769e7dbebb3d6f62b5ce182cd
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-14 12:17:52 +0100

    new feat: safe child app

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+//Forms script goes in here
+
+//capture form and email
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+//listen for when user submits email
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
