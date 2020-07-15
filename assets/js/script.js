3d7713594e0cf1b2704214075d7b9064f28cf61d
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-15 19:06:37 +0100

    feat: img pop-overs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,8 @@
-//Forms script goes in here
+$ (document).ready(function(){
+    $('.pop-up').click(function(event){
+        event.preventDefault();
+        $('.modal img').attr('src', $(this).attr('src'));
+        $('.modal').modal('show');
+    })
 
-//capture form and email
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-
-//listen for when user submits email
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    checkInputs();
-});
-
-
-function checkInputs() {
-    // trim to remove the whitespaces
-    const emailValue = email.value.trim();
-
-    if (emailValue === '') {
-        setErrorFor(email, 'Email cannot be blank');
-    } else if (!isEmail(emailValue)) {
-        setErrorFor(email, 'Not a valid email');
-    } else {
-        setSuccessFor(email);
-    }
-
-}
-
-function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
-
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    formControl.className = 'form-control error';
-    small.innerText = message;
-}
-
-function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-}
+})
