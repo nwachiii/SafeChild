f74a70ef47bfe4660390b03a66fabe4fadff0f31
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-16 14:36:19 +0100

    feat: email capture

--- assets/app.php
+++ assets/app.php
@@ -1,29 +1,40 @@
 <?php 
         
+	$email = $_POST['email'];
+
     // Only process the form if $_POST is not empty
     if ( ! empty($_POST)) {
 
         //connect to MySQL
-    $mysqli = new mysqli( 'localhost', 'root', '1sqlpassword@sql', 'users' );
+    // $mysqli = new mysqli( 'localhost', 'sql2354768', 'rZ2*hT3!', 'sql2354768' );
+    	$conn = new mysqli( 'localhost', 'root', '', 'sql2354768' );
 
         //Check our connection.
-    if ( mysqli -> connect_error ) {
-        die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
+    if ( $conn -> connect_error ) {
+        die( 'Connection Failed : '.$conn->connect_errno . ': '.$conn->connect_error);
+    }else {
+    	$stmt = $conn->prepare("insert into subscribers(email) values(?)");
+    	$stmt->bind_param("s", $email);
+    	$stmt->execute();
+    	echo "Registration Successful! You'll get notified when SafeChild App is launched";
+    	$stmt->close();
+    	$conn->close();
+
     }
 
-        //Insert form data
-    $sql = "INSERT INTO users (emails) VALUES ( '{$mysqli->real_escape_string($_POST['email'])}')";
-    $insert = $mysqli->query($sql);
+    //     //Insert form data
+    // $sql = "INSERT INTO subscribers (email) VALUES ( '{$mysqli->real_escape_string($_POST['email'])}')";
+    // $insert = $mysqli->query($sql);
     
-        //Print response from MySQL
-    if ( $insert ) {
-        echo "Success! Row ID: {$mysqli->insert_id}";
-    } else {
-        die("Error: {$mysqli->errno} : {$mysqli->error}");
-    }
+    //     //Print response from MySQL
+    // if ( $insert ) {
+    //     echo "Success! Row ID: {$mysqli->insert_id}";
+    // } else {
+    //     die("Error: {$mysqli->errno} : {$mysqli->error}");
+    // }
 
-        //close connection
-    $mysqli->close();
+    //     //close connection
+    // $mysqli->close();
 
 }
 ?>
---
+++ assets/form.php
@@ -0,0 +1,30 @@
+<?php 
+        
+    $email = $_POST['email'];
+
+        //connect to MySQL
+        $conn = new mysqli( 'sql2.freesqldatabase.com', 'sql2354768', 'rZ2*hT3!', 'sql2354768' );
+
+        //Check our connection.
+    if ( $conn -> connect_error ) {
+        die( 'Connection Failed : '.$conn->connect_error);
+    }else {
+        $sql = "INSERT INTO subscribers(email) VALUES ( '{$conn->real_escape_string($_POST['email'])}')";
+
+        $insert = $conn->query($sql);
+    
+        //Print response from MySQL
+        if ( $insert ) {
+        echo "Registration Successful! You'll get notified when SafeChild App is launched";
+        } else{
+            echo "registration failed";
+        }
+
+        $sql->close();
+        $conn->close();
+
+        header("refresh:2; url=index.html");
+
+    }
+
+?>
