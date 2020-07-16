148b0971ae9de6616c1953df861d55fb6af17eef
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-16 22:45:40 +0100

    feat: new design changes

--- assets/form.php
+++ assets/form.php
@@ -5,6 +5,7 @@
         //connect to MySQL
         $conn = new mysqli( 'sql2.freesqldatabase.com', 'sql2354768', 'rZ2*hT3!', 'sql2354768' );
 
+
         //Check our connection.
     if ( $conn -> connect_error ) {
         die( 'Connection Failed : '.$conn->connect_error);
